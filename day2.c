1772fc289e743a467d9948b2907300755acc0602
risira <risira@stekz.com>
2026-03-11 15:56:03 +0530

    code-review-day2

--- day2.c
+++ day2.c
@@ -3,11 +3,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define MAX 256
+#define MAX_RULE 128
+#define MAX_PASSWORD 256
 
 typedef struct {
-  char rule[MAX]; 
-  char password[MAX]; 
+  char rule[MAX_RULE]; 
+  char password[MAX_PASSWORD]; 
 } PasswordSegment;
 
 typedef struct {
@@ -16,8 +17,17 @@ typedef struct {
   char letter;
 } PasswordRule; 
 
+
+bool in_bounds(int min, int max, int v){
+  return v >= min && v <= max; 
+}
+
 PasswordSegment get_password_segment(char* line){
   int n = strlen(line);
+  if (!in_bounds(0, MAX_PASSWORD, n)){
+    printf("password '%s' too long\n", line); 
+    exit(1);
+  }
   int k = 0;
   bool isRule = true;
 
@@ -30,17 +40,17 @@ PasswordSegment get_password_segment(char* line){
       isRule = false;
       k = 0; 
     }else{
-      if (isRule)
+      if (isRule && in_bounds(0, MAX_RULE-1, k))
         seg.rule[k++] = c;
       else
         {
-          if (c != ' '){
+          if (c != ' ' && in_bounds(0, MAX_PASSWORD-1, k)){
              seg.password[k++] = c;
           }
         } 
     }
   }
-  seg.password[k++] ='\0';
+  seg.password[k] ='\0';
 
   return seg; 
 }
@@ -49,21 +59,25 @@ PasswordRule get_password_rule(PasswordSegment * seg){
   PasswordRule rule = {0}; 
   int r = sscanf(seg->rule, "%d-%d %c", &rule.min, &rule.max, &rule.letter);
   if (r != 3){
-    printf("error: failed on %s\n", seg->password);
+    printf("error: failed on rule:%s, password: %s\n", seg->rule, seg->password);
     exit(1); 
   }
   return rule; 
 }
 
 bool is_valid(PasswordSegment * seg, PasswordRule * rule){
-  int count = 0; 
-  for (int i = 0; i < strlen(seg->password); i++){
+  int count = 0;
+  int n = strlen(seg->password); 
+  for (int i = 0; i < n; i++){
     count += seg->password[i] == rule->letter ? 1 : 0; 
   }
   return count >= rule->min && count <= rule->max;
 }
 
+
 bool is_valid_2(PasswordSegment * seg, PasswordRule * rule){
+  int len = strlen(seg->password);
+  if (in_bounds(rule->min-1, rule->max-1, len)) return false;
   char l1 = seg->password[rule->min-1];
   char l2 = seg->password[rule->max-1];
   bool m1 = l1 == rule->letter;
@@ -76,12 +90,13 @@ bool is_valid_2(PasswordSegment * seg, PasswordRule * rule){
 void act_problem(char * file_name, bool (*check)(PasswordSegment *, PasswordRule *)){
   FILE *f = fopen(file_name, "r");
   if (f == NULL){
-    perror("error opening file 'day2.txt', please ensure problem statement exists");
+    fprintf(stderr, "error opening file '%s'\n", file_name); 
     exit(1); 
   }
-  char line[MAX];
+  char line[MAX_RULE+MAX_PASSWORD];
   int count = 0; 
   while(fgets(line,sizeof(line), f) != NULL){
+    line[strcspn(line, "\n")] = '\0';
     PasswordSegment seg = get_password_segment(line);
     PasswordRule rule = get_password_rule(&seg); 
     count += check(&seg, &rule) ? 1 : 0; 
